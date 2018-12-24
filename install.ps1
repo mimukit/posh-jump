c8805fb49dcbeb2e1a3a4db41647c2c0e32578f0
Mukitul Islam Mukit <hello@mukit.me>
2018-12-25 02:07:45 +0600

    add module import during installation

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
 Copy-Item . "~\Documents\WindowsPowerShell\Modules" -r -Force
 
+Import-Module ~\Documents\WindowsPowerShell\Modules\posh-jump\posh-jump.psd1
+
 Write-Host "Install successful. Restart powershell now"
